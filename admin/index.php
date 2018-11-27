e79c2c6ba190e0278048174d05cec8acca3318b1
ladada200 <misfitanimations@gmail.com>
2018-11-27 13:34:44 -0400

    major, added
    Updated htaccess
    updated method action in handleAccount
    updated index
    Added main.js for testing

--- admin/index.php
+++ admin/index.php
@@ -4,18 +4,23 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+
+session_start();
+
+if ($_SESSION['user'] !== "super") {
+    header('location: ../');
+}
+
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link rel="stylesheet" href="lib/main.css" />
-        <script src="main.js" type="text/javascript"></script>
     </head>
     <body>
         <?php
         // put your code here
-        
         ?>
-
     </body>
 </html>
---
+++ main.js
@@ -0,0 +1,36 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+window.onload = function() {
+    gettempuser();
+}
+
+function gettempuser() {
+    var url = "account/";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var resp = JSON.parse(this.responseText);
+            console.log(resp);
+        }
+    };
+    xmlhttp.open("tempUser", url, true);
+    //xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send();
+    //xmlhttp.send(JSON.stringify(obj));
+}
+
+/*
+var car = document.querySelector(".highlighted").querySelectorAll("td");
+     var obj = {
+        carID: car[0].innerHTML,
+        carYear: car[1].innerHTML,
+        carMake: car[2].innerHTML,
+        carModel: car[3].innerHTML,
+        carColour: car[4].innerHTML,
+        carPrice: car[5].innerHTML
+    }
+*/  
